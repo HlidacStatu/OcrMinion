994adbeb23548067503027c791ba28b9bb555e2a
suchoss <suchoss@gmail.com>
2020-01-09 11:04:34 +0100

    Finalizing unit tests

--- Tests/RestClientTests.cs
+++ Tests/RestClientTests.cs
@@ -133,5 +133,208 @@ namespace Tests
             result.Should().Be(content);
 
         }
+
+        [Fact(DisplayName = "Check if SendResultAsync is successfully completed")]
+        public async Task SendResultAsyncCorrectResponse()
+        {
+            // ARRANGE
+            // prepare what is going to be returned by httpclient
+            string content = "";
+            var httpClient = CreateHttpClientMock(HttpStatusCode.OK, content);
+
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            Document doc = new Document(
+                "docId", 
+                new DateTime(2020, 2, 22), 
+                new DateTime(2020, 3, 22), 
+                "someFilename", 
+                "someText", 
+                "200", 
+                "some errror");
+
+            // ACT
+            var taskResult = rc.SendResultAsync("taskid123", doc, new CancellationToken());
+            await taskResult;
+
+            // ASSERT
+            taskResult.IsCompletedSuccessfully.Should().BeTrue();
+        }
+
+        [Fact(DisplayName = "Check if CancelTaskAsync is successfully completed")]
+        public async Task CancelTaskAsyncCorrectResponse()
+        {
+            // ARRANGE
+            // prepare what is going to be returned by httpclient
+            string content = "";
+            var httpClient = CreateHttpClientMock(HttpStatusCode.OK, content);
+
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            // ACT
+            var taskResult = rc.CancelTaskAsync("taskid123");
+            await taskResult;
+
+            // ASSERT
+            taskResult.IsCompletedSuccessfully.Should().BeTrue();
+        }
+
+
+        [Fact(DisplayName = "Check if GetTaskAsync throws ServerHasNoTasksException")]
+        public void GetTaskAsyncThrowsServerHasNoTasksException()
+        {
+            // ARRANGE
+            string errorResult = "{\"error\":\"some error\", \"nextRequestInSec\":10}";
+
+            // prepare what is going to be returned by httpclient
+            var httpClient = CreateHttpClientMock((HttpStatusCode)420, errorResult);
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            FluentActions.Invoking(() => rc.GetTaskAsync(new CancellationToken())).Should().Throw<ServerHasNoTasksException>();
+
+        }
+
+        [Theory(DisplayName = "Check if GetTaskAsync throws BlockedByServerException")]
+        [InlineData(HttpStatusCode.Unauthorized)]
+        [InlineData(HttpStatusCode.Forbidden)]
+        public void GetTaskAsyncThrowsBlockedByServerException(HttpStatusCode expectedStatusCode)
+        {
+            // ARRANGE
+            string errorResult = "{\"error\":\"some error\", \"nextRequestInSec\":10}";
+
+            // prepare what is going to be returned by httpclient
+            var httpClient = CreateHttpClientMock(expectedStatusCode, errorResult);
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            FluentActions.Invoking(() => rc.GetTaskAsync(new CancellationToken())).Should().Throw<BlockedByServerException>();
+
+        }
+
+        [Fact(DisplayName = "Check if GetFileToAnalyzeAsync throws ServerHasNoTasksException")]
+        public void GetFileToAnalyzeAsyncThrowsServerHasNoTasksException()
+        {
+            // ARRANGE
+            string errorResult = "{\"error\":\"some error\", \"nextRequestInSec\":10}";
+
+            // prepare what is going to be returned by httpclient
+            var httpClient = CreateHttpClientMock((HttpStatusCode)420, errorResult);
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            FluentActions.Invoking(() => rc.GetFileToAnalyzeAsync("1", new CancellationToken())).Should().Throw<ServerHasNoTasksException>();
+
+        }
+
+        [Theory(DisplayName = "Check if GetFileToAnalyzeAsync throws BlockedByServerException")]
+        [InlineData(HttpStatusCode.Unauthorized)]
+        [InlineData(HttpStatusCode.Forbidden)]
+        public void GetFileToAnalyzeAsyncThrowsBlockedByServerException(HttpStatusCode expectedStatusCode)
+        {
+            // ARRANGE
+            string errorResult = "{\"error\":\"some error\", \"nextRequestInSec\":10}";
+
+            // prepare what is going to be returned by httpclient
+            var httpClient = CreateHttpClientMock(expectedStatusCode, errorResult);
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            FluentActions.Invoking(() => rc.GetFileToAnalyzeAsync("1", new CancellationToken())).Should().Throw<BlockedByServerException>();
+
+        }
+
+        [Fact(DisplayName = "Check if SendResultAsync throws ServerHasNoTasksException")]
+        public void SendResultAsyncThrowsServerHasNoTasksException()
+        {
+            // ARRANGE
+            string errorResult = "{\"error\":\"some error\", \"nextRequestInSec\":10}";
+
+            // prepare what is going to be returned by httpclient
+            var httpClient = CreateHttpClientMock((HttpStatusCode)420, errorResult);
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            Document doc = new Document(
+                "docId",
+                new DateTime(2020, 2, 22),
+                new DateTime(2020, 3, 22),
+                "someFilename",
+                "someText",
+                "200",
+                "some errror");
+
+            FluentActions.Invoking(() => rc.SendResultAsync("1", doc, new CancellationToken())).Should().Throw<ServerHasNoTasksException>();
+
+        }
+
+        [Theory(DisplayName = "Check if SendResultAsync throws BlockedByServerException")]
+        [InlineData(HttpStatusCode.Unauthorized)]
+        [InlineData(HttpStatusCode.Forbidden)]
+        public void SendResultAsyncThrowsBlockedByServerException(HttpStatusCode expectedStatusCode)
+        {
+            // ARRANGE
+            string errorResult = "{\"error\":\"some error\", \"nextRequestInSec\":10}";
+
+            // prepare what is going to be returned by httpclient
+            var httpClient = CreateHttpClientMock(expectedStatusCode, errorResult);
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            Document doc = new Document(
+                "docId",
+                new DateTime(2020, 2, 22),
+                new DateTime(2020, 3, 22),
+                "someFilename",
+                "someText",
+                "200",
+                "some errror");
+
+            FluentActions.Invoking(() => rc.SendResultAsync("1", doc, new CancellationToken())).Should().Throw<BlockedByServerException>();
+
+        }
+
+        [Fact(DisplayName = "Check if CancelTaskAsync throws ServerHasNoTasksException")]
+        public void CancelTaskAsyncThrowsServerHasNoTasksException()
+        {
+            // ARRANGE
+            string errorResult = "{\"error\":\"some error\", \"nextRequestInSec\":10}";
+
+            // prepare what is going to be returned by httpclient
+            var httpClient = CreateHttpClientMock((HttpStatusCode)420, errorResult);
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            Document doc = new Document(
+                "docId",
+                new DateTime(2020, 2, 22),
+                new DateTime(2020, 3, 22),
+                "someFilename",
+                "someText",
+                "200",
+                "some errror");
+
+            FluentActions.Invoking(() => rc.CancelTaskAsync("1")).Should().Throw<ServerHasNoTasksException>();
+
+        }
+
+        [Theory(DisplayName = "Check if CancelTaskAsync throws BlockedByServerException")]
+        [InlineData(HttpStatusCode.Unauthorized)]
+        [InlineData(HttpStatusCode.Forbidden)]
+        public void CancelTaskAsyncThrowsBlockedByServerException(HttpStatusCode expectedStatusCode)
+        {
+            // ARRANGE
+            string errorResult = "{\"error\":\"some error\", \"nextRequestInSec\":10}";
+
+            // prepare what is going to be returned by httpclient
+            var httpClient = CreateHttpClientMock(expectedStatusCode, errorResult);
+            var rc = new RestClient(httpClient, CreateIOptionsMonitorMock(), CreateILoggerMock());
+
+            Document doc = new Document(
+                "docId",
+                new DateTime(2020, 2, 22),
+                new DateTime(2020, 3, 22),
+                "someFilename",
+                "someText",
+                "200",
+                "some errror");
+
+            FluentActions.Invoking(() => rc.CancelTaskAsync("1")).Should().Throw<BlockedByServerException>();
+
+        }
+
     }
 }
